0b854d8df758565b592dbe676aafb458f30f9203
petr.kiyashko <petr.kiyashko@gaijin.team>
2026-07-08 19:01:31 +0200

    m

---
+++ dotfiles/nvim/after/plugin/fff_setup.lua
@@ -0,0 +1,14 @@
+local ok, fff = pcall(require, "fff")
+
+if ok then
+    fff.setup({})
+    vim.keymap.set("n", "<C-f>", fff.find_files, {
+        desc = "Find files",
+    })
+    vim.keymap.set("n", "<C-S-f>", fff.live_grep, {
+        desc = "Live grep",
+    })
+    vim.keymap.set("n", "<C-b>", fff.buffers, {
+        desc = "Switch buffers",
+    })
+end
